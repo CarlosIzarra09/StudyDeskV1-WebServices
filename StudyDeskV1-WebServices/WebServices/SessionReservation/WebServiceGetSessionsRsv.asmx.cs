2853b47d5817801086a42d2f44c44a2f2c5813bf
CarlosIzarra09 <eduizarra2@hotmail.com>
2022-06-28 13:01:15 -0500

    feat: Add CRUD for session-reservation

---
+++ StudyDeskV1-WebServices/WebServices/SessionReservation/WebServiceGetSessionsRsv.asmx.cs
@@ -0,0 +1,72 @@
+﻿using StudyDeskV1_WebServices.Helper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+
+namespace StudyDeskV1_WebServices.WebServices.SessionReservation
+{
+    /// <summary>
+    /// Descripción breve de WebServiceGetSessionsRsv
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebServiceGetSessionsRsv : System.Web.Services.WebService
+    {
+
+        string consulta, uid, password, server, database;
+        private SqlConnection connection;
+        readonly DataSet dataTable = new DataSet();
+        public AuthHeader credentials;
+
+        public WebServiceGetSessionsRsv()
+        {
+            Initialize();
+        }
+        private void Initialize()
+        {
+            server = "sql202201.database.windows.net";
+            database = "studydeskDb";
+            uid = "STUDYDESK";
+            password = "8CL7cR$Ce$gCxNmB";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+            connection = new SqlConnection(connectionString);
+        }
+
+        [WebMethod]
+        [SoapHeader("credentials")]
+        public DataSet ListaSesionesReservadas()
+        {
+            if (credentials != null)
+            {
+                if (credentials.IsValid())
+                {
+                    connection.Open();
+                    consulta = "SELECT * FROM dbo.session_reservations";
+
+                    SqlDataAdapter sqlAdapter = new SqlDataAdapter(consulta, connection);
+                    sqlAdapter.Fill(dataTable, "SessionReservations");
+
+                    connection.Close();
+                    return dataTable;
+                }
+                else
+                    return dataTable;
+            }
+            else
+            {
+                return dataTable;
+            }
+        }
+    }
+}
---
+++ StudyDeskV1-WebServices/WebServices/SessionReservation/WebServicePostSessionRsv.asmx.cs
@@ -0,0 +1,92 @@
+﻿using StudyDeskV1_WebServices.Communications;
+using StudyDeskV1_WebServices.Helper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+
+namespace StudyDeskV1_WebServices.WebServices.SessionReservation
+{
+    /// <summary>
+    /// Descripción breve de WebServicePostSessionRsv
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebServicePostSessionRsv : System.Web.Services.WebService
+    {
+
+        string consulta, uid, password, server, database;
+        private SqlConnection connection;
+        readonly DataSet dataTable = new DataSet();
+        public AuthHeader credentials;
+
+        public WebServicePostSessionRsv()
+        {
+            Initialize();
+        }
+        private void Initialize()
+        {
+            server = "sql202201.database.windows.net";
+            database = "studydeskDb";
+            uid = "STUDYDESK";
+            password = "8CL7cR$Ce$gCxNmB";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+            connection = new SqlConnection(connectionString);
+        }
+
+
+        [WebMethod]
+        [SoapHeader("credentials")]
+        public WsSecurityResponse InsertarSesionReservada(int studentId,int sessionId,int qualification,bool confirmed)
+        {
+            if (credentials != null)
+            {
+                if (credentials.IsValid())
+                {
+                    connection.Open();
+                    string result;
+
+                    SqlCommand cmd = new SqlCommand("INSERT INTO dbo.session_reservations(student_id, session_id,qualification, confirmed) values(@studentId, @sessionId,@qualification,@confirmed)", connection);
+                    cmd.Parameters.AddWithValue("@studentId", studentId);
+                    cmd.Parameters.AddWithValue("@sessionId", sessionId);
+                    cmd.Parameters.AddWithValue("@qualification", sessionId);
+                    cmd.Parameters.AddWithValue("@confirmed", confirmed);
+                    try
+                    {
+                        cmd.ExecuteNonQuery();
+                        result = "A Session Reservation was inserted without problems";
+                        connection.Close();
+                        return new WsSecurityResponse(null, result);
+                    }
+                    catch (Exception ex)
+                    {
+                        result = "An error occurred while a Session Reservation was being inserted: " + ex.ToString();
+                        connection.Close();
+                        return new WsSecurityResponse(result);
+                    }
+
+
+                }
+                else
+                    return new WsSecurityResponse("Service failed to authenticate against the provided profile credentials. " +
+                        "Verify that the SOAP request is using the proper credentials.");
+            }
+            else
+            {
+                return new WsSecurityResponse("The selected Security policy either does not have any " +
+                    "security profiles (X509 or UserNameToken) or the security profiles are " +
+                    "inactive. Verify at least one security profile is active.");
+            }
+        }
+    }
+}
